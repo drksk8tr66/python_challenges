a791a10fd6b6793f2f5675445d65dbe4d5064daf
Dustin Brown <dust7667@RACKSPACE.COM>
2016-07-15 12:12:13 -0500

    upload of challenge 6 and Nicole Code

--- challenges/challenge_5/Ch #5 - Nicole.py
+++ challenges/challenge_5/Ch #5 - Nicole.py
@@ -7,23 +7,23 @@ https://en.wikipedia.org/wiki/Chudnovsky_algorithm
 '''
 
 import math
-import decimal
-
+from decimal import Decimal, getcontext
+    
 
 def chudnovsky(i, n):
     # Using the decimal module and setting precision (adding ten to prevent rounding errors)
     # This prevents us from losing precision/accuracy in calculations
-    decimal.getcontext().prec = n + 10
+    getcontext().prec = n + 10
 
     # Calculating the summation portion of the Chudnovsky algorithm
     s = 0
     for k in range(0, i):
-        s += decimal.Decimal(((-1) ** k * math.factorial(6 * k) * (545140134 * k + 13591409)) / decimal.Decimal(
+        s += Decimal(((-1) ** k * math.factorial(6 * k) * (545140134 * k + 13591409)) / Decimal(
             math.factorial(3 * k) * math.factorial(k) ** 3 *
-            decimal.Decimal((640320 ** 3) ** (decimal.Decimal(k + decimal.Decimal(0.5))))))
+            Decimal((640320 ** 3) ** (Decimal(k + Decimal(0.5))))))
 
     # Calculating the rest of the Chudnovsky algorithm and returning the results
-    pi = decimal.Decimal(1 / decimal.Decimal(12 * s))
+    pi = Decimal(1 / Decimal(12 * s))
     return round(pi, n)
 
 
@@ -34,7 +34,7 @@ def main():
     num_digits = 'unknown'
     while not num_digits.isnumeric():
         num_digits = input('How many decimal digits of pi would you like to generate? ')
-        if num_digits in ('Q', 'q'):
+        if num_digits.upper() == 'Q':
             exit()
         if not num_digits.isnumeric():
             print('Valid number not entered.')
