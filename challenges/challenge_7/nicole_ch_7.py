30e216c8bc4987a684700c01621cc74dec091332
Nicole Smith <nicole.smith@rackspace.com>
2016-07-29 11:02:51 -0700

    Update nicole_ch_7.py

--- challenges/challenge_7/nicole_ch_7.py
+++ challenges/challenge_7/nicole_ch_7.py
@@ -5,30 +5,19 @@ https://www.reddit.com/r/dailyprogrammer/comments/pserp/2162012_challenge_8_easy
 
 
 def main():
-    # Loop through each number of bottles
-    for i in range(1, 100):
-        # Number of bottles
-        bottles = 100 - i
-        next_bottles = bottles - 1
-        end = ", "
+    # Repeating parts of the song
+    part_1 = ' bottles of beer'
+    part_1n = ' bottle of beer'
+    part_2 = ' on the wall'
+    part_3 = ' take one down and pass it around'
 
-        # Being gramatically correct
-        if bottles == 1:
-            bottles_word = 'bottle'
-        else:
-            bottles_word = 'bottles'
-        if next_bottles == 1:
-            next_bottles_word = 'bottle'
-        else:
-            next_bottles_word = 'bottles'
-        if next_bottles == 0:
-            next_bottles = 'no more'
-            end = ""
+    # Loop for everything until we get to a part with one bottle
+    for i in range(99, 2, -1):
+        print('{0}{1}{2}, {0}{1},{3}, {4}{1}{2}'.format(i, part_1, part_2, part_3, i - 1), end=", ")
 
-        # Print to screen
-        print('{0} {1} of beer on the wall, {0} {1} of beer, take one down and pass it around, {2} {3} of '
-              'beer on the wall'.format(bottles, bottles_word, next_bottles, next_bottles_word),
-              end=end)
+    # Displaying one bottle so it is gramatically correct
+    print('{0}{1}{2}, {0}{1},{3}, {4}{5}{2}'.format(2, part_1, part_2, part_3, 1, part_1n), end=", ")
+    print('{0}{1}{2}, {0}{1},{3}, {4}{5}{2}'.format(1, part_1n, part_2, part_3, 'no more', part_1))
 
 
 if __name__ == "__main__":
