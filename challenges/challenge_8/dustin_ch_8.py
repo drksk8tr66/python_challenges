91e5eeb6b8840a4cdf0a0befa02058a528d14c79
Dustin Brown <dust7667@RACKSPACE.COM>
2016-08-25 11:14:58 -0500

    updating dustins unit tests

--- challenges/challenge_8/dustin_ch_8.py
+++ challenges/challenge_8/dustin_ch_8.py
@@ -1,31 +1,31 @@
+import cmath
 
-
-def arrange_message(words: int=0, msg: str=None):
-    if not msg:
-        msg = input("please type you message here")
-    if msg.isnumeric():
-        digits = []
-        for i in range(len(str(msg))):
-            digits.append(str(msg)[i])
-            digits.sort()
-        print(digits)
-        return digits
-    else:
-        if words == 0:
-            new_msg = sorted([msg.split(' ')])
-            print(new_msg)
-            return new_msg
+def arrange_msg(msg):
+    nums = [int, float]
+    chars = []
+    digits = []
+    sorted_msg = []
+    for i in msg:
+        if type(i) in nums:
+            digits.append(i)
+        elif type(i) == complex:
+            digits.append(cmath.phase(i))
         else:
-            chars = []
-            for i in range(len(msg)):
-                chars.append(msg[i])
-                chars.sort()
-            print(chars)
-            return chars
+            chars.append(i)
+    chars.sort()
+    digits.sort()
+    if digits:
+        sorted_msg = digits
+        for i in chars:
+            sorted_msg.append(i)
+    elif chars:
+        sorted_msg = chars
+    return sorted_msg
+
 
 
 if __name__ == '__main__':
-    arrange_message(1, "123")
+    arrange_msg([1, 2, 3, 'a', 'b', 'c'])
 
 
 
--- challenges/challenge_9/dustin_ch_9.py
+++ challenges/challenge_9/dustin_ch_9.py
@@ -1,18 +1,65 @@
 import unittest
-from challenges.challenge_8.dustin_ch_8 import arrange_message
-
+from challenges.challenge_8.dustin_ch_8 import arrange_msg
+from challenges.challenge_8.nicole_ch_8 import sort_list
+from challenges.challenge_8.jj_ch_8 import order_list
 
 class MyTestCase(unittest.TestCase):
 
     def test_only_numbers_ordered(self):
-        x = '123'
-        expected = ['1', '2', '3']
-        self.assertEquals(arrange_message(1, x), expected)
+        x = [1, 2, 3]
+        expected = [1, 2, 3]
+        self.assertEquals(arrange_msg(x), expected)
+        self.assertEquals(sort_list(x), expected)
+        self.assertEquals(order_list(x), expected)
 
     def test_only_numbers_unordered(self):
-        x = '312'
-        expected = ['1', '2', '3']
-        self.assertEquals(arrange_message(1, x), expected)
+        x = [2, 1, 3]
+        expected = [1, 2, 3]
+        self.assertEquals(arrange_msg(x), expected)
+        self.assertEquals(sort_list(x), expected)
+        self.assertEquals(order_list(x), expected)
+
+    def test_only_string_ordered(self):
+        x = '123abc'
+        expected = ['1', '2', '3', 'a', 'b', 'c']
+        self.assertEquals(arrange_msg(x), expected)
+        self.assertEquals(sort_list(x), expected)
+        self.assertEquals(order_list(x), expected)
+
+    def test_only_string_unordered(self):
+        x = 'acb132'
+        expected = ['1', '2', '3', 'a', 'b', 'c']
+        self.assertEquals(arrange_msg(x), expected)
+        self.assertEquals(sort_list(x), expected)
+        self.assertEquals(order_list(x), expected)
+
+    def test_int_string_ordered(self):
+        x = [1, 2, 3, 'a', 'b', 'c']
+        expected = [1, 2, 3, 'a', 'b', 'c']
+        self.assertEquals(arrange_msg(x), expected)
+        self.assertEquals(sort_list(x), expected)
+        self.assertEquals(order_list(x), expected)
+
+    def test_int_string_unordered(self):
+        x = [3, 'a', 2, 'c', 'b', 1]
+        expected = [1, 2, 3, 'a', 'b', 'c']
+        self.assertEquals(arrange_msg(x), expected)
+        self.assertEquals(sort_list(x), expected)
+        self.assertEquals(order_list(x), expected)
+
+    def test_complex_ordered(self):
+        x = [(1.0+1.0j), (-2.0+1.0j), (-1.0+0.0j)]
+        expected = [0.7853981633974483, 2.677945044588987, 3.141592653589793]
+        self.assertEquals(arrange_msg(x), expected)
+        self.assertEquals(sort_list(x), expected)
+        self.assertEquals(order_list(x), expected)
+
+    def test_complex_unordered(self):
+        x = [(-1.0 + 0.0j), (1.0 + 1.0j), (-2.0 + 1.0j)]
+        expected = [0.7853981633974483, 2.677945044588987, 3.141592653589793]
+        self.assertEquals(arrange_msg(x), expected)
+        self.assertEquals(sort_list(x), expected)
+        self.assertEquals(order_list(x), expected)
 
 
 if __name__ == '__main__':
