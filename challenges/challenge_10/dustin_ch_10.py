8578e4c2111c43efbe6bd23e2f07b8fef4d833a0
Dustin Brown <dust7667@RACKSPACE.COM>
2016-08-30 10:57:41 -0500

    updated Dustins code

--- challenges/challenge_10/dustin_ch_10.py
+++ challenges/challenge_10/dustin_ch_10.py
@@ -6,11 +6,20 @@ def validate(t_num):
         return False
     if type(t_num) is not str:
         return False
-    if re.match("[0-9]{10}|[0-9]{7}|([0-9]{3}-)?[0-9]{3}-[0-9]{4}|[0-9]{3}\.[0-9]{3}\.[0-9]{4}|"
-                "\([0-9]{3}\) ?[0-9]{3}-[0-9]{4}", t_num):
+    if re.match("[0-9]{10}$|[0-9]{7}$|([0-9]{3}-)?[0-9]{3}-[0-9]{4}$|[0-9]{3}\.[0-9]{3}\.[0-9]{4}$|"
+                "\([0-9]{3}\) ?[0-9]{3}-[0-9]{4}$", t_num):
         return True
     else:
         return False
 
+
 if __name__ == '__main__':
     print(validate('1234567890'))
+    print(validate('123-456-7890'))
+    print(validate('123.456.7890'))
+    print(validate('(123)456-7890'))
+    print(validate('(123) 456-7890'))
+    print(validate('456-7890'))
+    print(validate('123-45-6789'))
+    print(validate('123:4567890'))
+    print(validate('123/456-7890'))
