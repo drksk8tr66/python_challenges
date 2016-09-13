6974a0f00478408d4b3a9ce5d65d0c8f94eb608b
Dustin Brown <dust7667@RACKSPACE.COM>
2016-09-13 11:47:18 -0500

    uploading unit tests for ch 10

--- challenges/challenge_10/dustin_ch_10.py
+++ challenges/challenge_10/dustin_ch_10.py
@@ -6,14 +6,21 @@ def validate(t_num):
         return False
     if type(t_num) is not str:
         return False
-    if re.match("[0-9]{3}-? ?[0-9]{3}-? ?[0-9]{4}$|[0-9]{3} ?[0-9]{4}$|([0-9]{3}-)?[0-9]{3}-[0-9]{4}$|"
-                "[0-9]{3}\.[0-9]{3}\.[0-9]{4}$|\([0-9]{3}\) ?[0-9]{3}-[0-9]{4}$", t_num):
+    if re.match("[0-9]{3}-? ?[0-9]{3}-? ?[0-9]{4}$|[0-9]{3} ?[0-9]{4}$|[0-9]{3}\.?[0-9]{4}$|"
+                "([0-9]{3}-)?[0-9]{3}-[0-9]{4}$|[0-9]{3}\.[0-9]{3}\.[0-9]{4}$|"
+                "\([0-9]{3}\) ?[0-9]{3}-?[0-9]{4}$", t_num):
         return True
     else:
         return False
 
 
 if __name__ == '__main__':
+    print(validate('123:4567'))
+    print(validate('(123)4567890'))
+    print(validate('123.4567'))
+    print(validate('123-456.7890'))
+    print(validate('123.456-7890'))
+    print(validate('1234567'))
     print(validate('123456-7890'))
     print(validate('123-456-7890'))
     print(validate('123.456.7890'))
@@ -23,4 +30,3 @@ if __name__ == '__main__':
     print(validate('123-45-6789'))
     print(validate('123:4567890'))
     print(validate('123/456-7890'))
-
