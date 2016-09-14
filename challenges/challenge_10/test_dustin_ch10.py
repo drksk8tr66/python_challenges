0fa18eae6113930083d5b8b7ff1b1c3cf8a09080
Dustin Brown <dust7667@RACKSPACE.COM>
2016-09-14 13:42:59 -0500

    updated dustins unit tests

--- challenges/challenge_10/test_dustin_ch10.py
+++ challenges/challenge_10/test_dustin_ch10.py
@@ -8,144 +8,143 @@ class MyTestCase(unittest.TestCase):
 
     def test_only_7_digits(self):
         x = '1234567'
-        z = PhoneCheck()
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        self.assertEquals(z.validate(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_only_10_digits(self):
         x = '1234567890'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_7_digits_hyphen(self):
         x = '123-4567'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_10_digits_1_hyphen(self):
         x = '123456-7890'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_10_digits_2_hyphen(self):
         x = '123-456-7890'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_10_digits_parens(self):
         x = '(123)4567890'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_10_digits_parens_1_hyphen(self):
         x = '(123)456-7890'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_10_digits_parens_space_hyphen(self):
         x = '(123) 456-7890'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_10_digits_spaces(self):
         x = '123 456 7890'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_7_digits_dots(self):
         x = '123.4567'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_10_digits_dots(self):
         x = '123.456.7890'
         expected = True
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_6_digits(self):
         x = '123456'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_8_digits(self):
         x = '12345678'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_7_characters(self):
         x = 'abcdefg'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_11_digits(self):
         x = '12345678901'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_9_digits(self):
         x = '123-45-6789'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_7_digits_colon(self):
         x = '123:4567'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_7_digits_slash(self):
         x = '123/4567'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_10_digits_mixed(self):
         x = '123-456.7890'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
     def test_invalid_10_digits_mixed_reversed(self):
         x = '123.456-7890'
         expected = False
         self.assertEquals(validate(x), expected)
         self.assertEquals(validate_phone_number(x), expected)
-        # self.assertEquals(order_list(x), expected)
+        self.assertEquals(PhoneCheck().validate(x), expected)
 
 
 if __name__ == '__main__':
