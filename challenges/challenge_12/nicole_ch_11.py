c367d957670f793ef658d0d591e1ecd81f65bba6
Nicole Smith <nicole.smith@rackspace.com>
2016-09-28 13:43:45 -0700

    add nicole's challenge 12

---
+++ challenges/challenge_12/nicole_ch_11.py
@@ -0,0 +1,20 @@
+"""
+Text Adventure Game
+https://www.reddit.com/r/dailyprogrammer/comments/pjbuj/intermediate_challenge_2/
+"""
+
+from challenges.challenge_12 import maze
+import pickle  # Allows us to read a dictionary from a file
+
+
+if __name__ == "__main__":
+    # Using the maze class and object
+    m = maze.Maze()
+
+    # Setting coordinates from a dictionary stored in a txt file
+    with open('coordinates.txt', 'rb') as handle:
+        coordinates = pickle.loads(handle.read())
+        m.set_coordinates(coordinates, coordinates.get('Entry'), coordinates.get('Exit'))
+
+    # Displaying the game to the user
+    m.default_maze()
