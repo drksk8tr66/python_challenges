e2ca43ed8e69f58a7698752b62b45fb399817565
Nicole Smith <nicole.smith@rackspace.com>
2016-11-09 11:33:02 -0800

    Updates for position/location classes

--- challenges/challenge_12/game_final/text_adventure.py
+++ challenges/challenge_12/game_final/text_adventure.py
@@ -3,5 +3,16 @@ This is the main module for the game.
 All other modules should be imported here to be ran for the full game experience.
 """
 
+from challenges.challenge_12.game_final import position
+
 if __name__ == '__main__':
-    pass
+    # Using the position class and object to create a map
+    game = position.Location()
+
+    # Reading coordinates and locations from a text file
+    game.set_coordinates_and_locations_from_file('coordinates.txt')
+
+    # Displaying the game to the user
+    print('Enter text to describe the start of the text adventure game (the story).\n')
+    game.default_navigation()
+    print('\nEnter text to describe the end of the text adventure game (the story).')
