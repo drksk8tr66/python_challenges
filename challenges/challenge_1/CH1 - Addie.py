fe0f1cfb64447909da9a42b53d555ee2a39c8442
Addie Ramadurgakar <Aditi.Ramadurgakar@rackspace.com>
2019-03-01 11:01:57 -0600

    CH1 - Addie initial upload

---
+++ challenges/challenge_1/CH1 - Addie.py
@@ -0,0 +1,40 @@
+
+def get_name():
+    while True:
+        n = input("What is your name?")
+        if n.isalpha():
+            break
+        if n.isdigit() or n.isalnum():
+            print("That doesn't seem like a name. Please enter your name.")
+    return n
+
+
+def get_age():
+    while True:
+        a = input("How old are you?")
+        if a.isdigit():
+            break
+        if a.isalpha() or a.isalnum():
+            print("That doesn't seem like a number. Please enter your age.")
+    return a
+
+
+def get_user():
+    while True:
+        u = input("What is your username?")
+        if sum(c.isalpha() for c in u) == 4 and sum(c.isdigit() for c in u) == 4:
+            break
+        else:
+            print("That doesn't seem like a username. Please enter your username.")
+    return u
+
+
+name = get_name()
+age = get_age()
+user = get_user()
+
+
+print("Your name is %s, you are %s years old, your username is %s." % (name, age, user))
+
+f = open('Ch #1 - Addie.txt', 'w')
+f.write("Your name is %s, you are %s years old, your username is %s." % (name, age, user))
