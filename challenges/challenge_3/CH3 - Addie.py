9c6801c372ffaa9c7a441aa3b7940b1449c2c9ca
Addie Ramadurgakar <Aditi.Ramadurgakar@rackspace.com>
2019-04-12 00:35:01 -0500

    Addie Initial Upload

---
+++ challenges/challenge_3/CH3 - Addie.py
@@ -0,0 +1,8 @@
+intab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+outtab = "YPLTAVKREZGMSHUBXNCDIJFQOWypltavkrezgmshubxncdijfqow"
+trantab = str.maketrans(intab, outtab)
+
+with open('Secret Message_encrypted.txt', 'r') as f2:
+    s = f2.read()
+
+print(s.translate({ord(x): y for (x, y) in zip(outtab, intab)}))
