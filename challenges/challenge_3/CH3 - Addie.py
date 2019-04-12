34d743562a65af4f9a9873a8face1640f21596ec
Addie Ramadurgakar <Aditi.Ramadurgakar@rackspace.com>
2019-04-12 09:17:50 -0500

    Addie Initial Upload

--- challenges/challenge_3/CH3 - Addie.py
+++ challenges/challenge_3/CH3 - Addie.py
@@ -6,3 +6,4 @@ with open('Secret Message_encrypted.txt', 'r') as f2:
     s = f2.read()
 
 print(s.translate({ord(x): y for (x, y) in zip(outtab, intab)}))
+# print(s.translate({ord(x): y for (x, y) in zip(intab, outtab)}))
