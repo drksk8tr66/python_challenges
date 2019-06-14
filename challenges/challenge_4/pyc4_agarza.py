9504c1f4a135612ae4a63ddb726d8fa0d99cc5ed
Adam Garza <Adam.Garza@rackspace.com>
2019-06-14 09:54:35 -0500

    adding comments

--- challenges/challenge_4/pyc4_agarza.py
+++ challenges/challenge_4/pyc4_agarza.py
@@ -1,6 +1,7 @@
 import string
 import random
 
+# gather all printable characters
 print_all = list(string.printable[:94])
 print_num = list(string.printable[:10])
 print_lc = list(string.printable[10:36])
@@ -16,11 +17,13 @@ for x in range(1,pw_distinct):
 	pw_string = ''.join(pw)
 	pw_score = 0
 
+	# one point for special character, one point for number, etc.
 	if any(j in pw for j in print_lc): pw_score +=1
 	if any(j in pw for j in print_uc): pw_score +=1
 	if any(j in pw for j in print_num): pw_score += 1
 	if any(j in pw for j in print_sc): pw_score += 1
 
+	# score each password as lower, moderate, or strong
 	if pw_score <= 2: pw_strength = 'LOW'
 	elif pw_score == 3: pw_strength = 'MODERATE'
 	elif pw_score == 4: pw_strength = 'STRONG'
