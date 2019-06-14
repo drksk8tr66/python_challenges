4ab826430f24f8e2a53201188335e87dc48ad7a9
Adam Garza <Adam.Garza@rackspace.com>
2019-06-14 10:09:14 -0500

    adding comments

--- challenges/challenge_6/pyc6_agarza.py
+++ challenges/challenge_6/pyc6_agarza.py
@@ -38,35 +38,42 @@ morse_code = {'A':'.-',
               '9':'----.',
               ' ':' / '}
 
+# swap key/values in dict
 morse_decode = {value : key for key, value in morse_code.items()}
 
 r = input("Would you like to send (S) or receive (R) a message?")
 
+# send message
 if r.upper() == "S":
     
     schar = ""
     send_string = input("Please enter your message \n")
     
     for x in itertools.chain(send_string):
+        # iterate over each character and map to code
         schar = schar + morse_code[x]
     
     print("Your message is:\n%s" % schar)
 
+
+# receive message
 elif r.upper() == "R":
     
     rchar = ""
+    
     received = input("Please paste your code:").upper()
-    split_words = received.split(" / ")
+    split_words = received.split(" / ")  # split by word
     
     for x in split_words:
-        
-        split_chars = x.split(" ")
+
+        # split by coded character
+        split_chars = x.split(" ") 
         word_str = ""
         
         for y in split_chars:
-            
-            word_str = word_str + morse_decode[y]
-        
-        rchar = rchar + word_str + " "
+            # map code to character and add to the end of word string
+            word_str = word_str + morse_decode[y] 
+
+        rchar = rchar + word_str + " " # add each word to the message string
     
     print("Your message is:\n%s" % rchar)
