6bf1e5604c4e6fcb8e39eb7ca697b6c03726b190
Adam Garza <Adam.Garza@rackspace.com>
2019-06-17 15:24:23 -0500

    adding comments.

--- challenges/challenge_5/pyc5_agarza.py
+++ challenges/challenge_5/pyc5_agarza.py
@@ -6,29 +6,45 @@ import decimal
 import numpy as np
 
  # set precision
-decimal.getcontext().prec = 30
-dec = decimal.Decimal
 
-# initial values
-a0 = 1
-b0 = 1 / np.sqrt(dec(2))
-t0 = 1 / dec(4)
-p0 = 1
+def set_prec():
+    while True:
+        p = input('Set level of precision: ')
+        if p.isdigit():
+            return int(p)
+            break
+        else:
+            print('Please enter a whole number.')
 
-# iterate while values are more than desired the desired level of precision
-while np.abs(a0 - b0) > 10**(-30):
+def main():
+    
+    prec = set_prec()
+    dec = decimal.Decimal
+    decimal.getcontext().prec = prec
 
-	a1 = (a0 + b0)/2
-	b1 = np.sqrt(a0*b0)
-	t1 = t0 - p0*(a0 - a1)**2
-	p1 = 2 * p0
+    # initial values
+    a0 = 1
+    b0 = 1 / np.sqrt(dec(2))
+    t0 = 1 / dec(4)
+    p0 = 1
 
-	pi = (a1 + b1)**2 / (4*t1)
+    # iterate while values are more than desired the desired level of precision
+    while np.abs(a0 - b0) > 10**(-prec):
 
-	# reset values for next iteration
-	a0 = a1
-	b0 = b1
-	t0 = t1
-	p0 = p1
+        a1 = (a0 + b0)/2
+        b1 = np.sqrt(a0*b0)
+        t1 = t0 - p0*(a0 - a1)**2
+        p1 = 2 * p0
 
-print(pi)
+        pi = (a1 + b1)**2 / (4*t1)
+
+        # reset values for next iteration
+        a0 = a1
+        b0 = b1
+        t0 = t1
+        p0 = p1
+
+    print(pi)
+
+if __name__ == '__main__':
+    main()
