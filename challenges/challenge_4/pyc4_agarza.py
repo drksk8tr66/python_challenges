ebacd453ae5b6a37ce832ec4765ab4e662645397
Adam Garza <Adam.Garza@rackspace.com>
2019-06-17 14:59:44 -0500

    adding comments

--- challenges/challenge_4/pyc4_agarza.py
+++ challenges/challenge_4/pyc4_agarza.py
@@ -1,32 +1,64 @@
 import string
 import random
 
-# gather all printable characters
-print_all = list(string.printable[:94])
-print_num = list(string.printable[:10])
-print_lc = list(string.printable[10:36])
-print_uc = list(string.printable[36:62])
-print_sc = list(string.printable[62:94])
 
-pw_distinct = int(input('How many passwords would you like to generate?'))
-pw_length = int(input('Password Length:'))
+def num_pw(): # request generated passwords
+    while True:
+        
+        pw_distinct = input('How many passowrds would you like to generate? ')
+        if pw_distinct.isdigit():
+            return int(pw_distinct)
+            break
 
-for x in range(1,pw_distinct):
 
-	pw = random.sample(print_all,pw_length)
-	pw_string = ''.join(pw)
-	pw_score = 0
+def len_pw(): # set password length
+    while True:
+        pw_length = input('Password length: ')
+        if pw_length.isdigit():
+            return int(pw_length)
+            break
+    
 
-	# one point for special character, one point for number, etc.
-	if any(j in pw for j in print_lc): pw_score +=1
-	if any(j in pw for j in print_uc): pw_score +=1
-	if any(j in pw for j in print_num): pw_score += 1
-	if any(j in pw for j in print_sc): pw_score += 1
+def gen_pw(l): # generate passwords    
+    
+    print_all = list(string.printable)[:94]
+    pw = ''.join(random.choices(print_all, k=l))
+    
+    return pw
 
-	# score each password as lower, moderate, or strong
-	if pw_score <= 2: pw_strength = 'LOW'
-	elif pw_score == 3: pw_strength = 'MODERATE'
-	elif pw_score == 4: pw_strength = 'STRONG'
-	else: pw_strength = 'ERROR'	
+   
+def score_pw(p): # score password
+    
+    pw = p
+    
+    print_num = list(string.printable[:10])
+    print_lc = list(string.printable[10:36])
+    print_uc = list(string.printable[36:62])
+    print_sc = list(string.printable[62:])
 
-	print(pw_string + '  -  PASSWORD STRENGTH: ' + pw_strength)
+    pw_score = 0    
+    if any(j in pw for j in print_lc): pw_score += 1
+    if any(j in pw for j in print_uc): pw_score +=1
+    if any(j in pw for j in print_num): pw_score += 1
+    if any(j in pw for j in print_sc): pw_score += 1
+    
+    if pw_score <= 2: pw_strength = 'Password Strength is LOW'
+    elif pw_score == 3: pw_strength = 'Password Strength is MODERATE'
+    elif pw_score == 4: pw_strength = 'Password Strength is STRONG'
+    else: 'ERROR'
+    
+    return pw_strength
+
+
+def main():
+    
+    n = num_pw()
+    l = len_pw()
+    
+    for x in range(n):
+        pw = gen_pw(l)
+        score = score_pw(pw)
+        print('{}  -  {}'.format(pw, score))
+
+if __name__ == '__main__':
+    main()
