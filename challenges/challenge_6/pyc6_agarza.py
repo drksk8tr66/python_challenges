6a9b9085b7451469f024364bb8df92040b60458c
Adam Garza <Adam.Garza@rackspace.com>
2019-06-18 13:28:16 -0500

    added comments

--- challenges/challenge_6/pyc6_agarza.py
+++ challenges/challenge_6/pyc6_agarza.py
@@ -1,5 +1,4 @@
-import itertools
-
+""" morse dicts """
 morse_code = {'A':'.-',
               'B':'-...',
               'C':'-.-.',
@@ -36,44 +35,42 @@ morse_code = {'A':'.-',
               '7':'--...',
               '8':'---..',
               '9':'----.',
-              ' ':' / '}
+              ' ':'/'}
 
-# swap key/values in dict
 morse_decode = {value : key for key, value in morse_code.items()}
 
-r = input("Would you like to send (S) or receive (R) a message?")
 
-# send message
-if r.upper() == "S":
     
-    schar = ""
-    send_string = input("Please enter your message \n")
-    
-    for x in itertools.chain(send_string):
-        # iterate over each character and map to code
-        schar = schar + morse_code[x]
-    
-    print("Your message is:\n%s" % schar)
+def morse_receive():  
+    """ receive - dash/dot to character """
+    received = input('Please paste your code: ').upper()
+    split_chars = received.split(' ')
+    message = ''.join(map(morse_decode.get, split_chars))   
+    print(message)
+
+def morse_send():
+    """ send - character to dash/dot """
+    send = input('Please enter your message: ').upper()
+    message = ' '.join(map(morse_code.get, send))    
+    print(message)
 
 
-# receive message
-elif r.upper() == "R":
-    
-    rchar = ""
-    
-    received = input("Please paste your code:").upper()
-    split_words = received.split(" / ")  # split by word
-    
-    for x in split_words:
 
-        # split by coded character
-        split_chars = x.split(" ") 
-        word_str = ""
+def main():
+
+    while True:
+        send_rec = input('Would you like to send (S) or receive (R) a message? ')
+        if send_rec.upper() in ('S','R'):
+            break
+    
+    if send_rec == 'S':
+        morse_send()
         
-        for y in split_chars:
-            # map code to character and add to the end of word string
-            word_str = word_str + morse_decode[y] 
+    elif send_rec == 'R':
+        morse_receive()
+        
+    else: print('Error')
 
-        rchar = rchar + word_str + " " # add each word to the message string
     
-    print("Your message is:\n%s" % rchar)
+if __name__ == '__main__':
+    main()
